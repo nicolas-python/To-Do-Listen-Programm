1df5563166f054b7954165615eccaf9ab573742e
Nicolas <nicolasmader001@gmail.com>
2026-03-08 15:00:11 +0100

    funktionen erstellt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #To-Do-Listen-Programm
 #menü
+to_do_liste= []
+
 def to_do_list_menue():
     choice = None
 
@@ -16,4 +18,56 @@ def to_do_list_menue():
             print("Ungültige Option, bitte erneut wählen.")
 
     return choice
+
+#funktionen
+def anzeigen():
+
+    if to_do_liste == []:
+        print("Keine Aufgaben mehr")
+
+    else :
+        print ("Deine Aufgaben:")
+        for aufgaben in to_do_liste:
+            print(aufgaben)
+
+
+
+def hinzufuegen():
+        print("Was möchtest du hinzufügen? :")
+        aufgabe = input()
+        to_do_liste.append(aufgabe)
+        print("Aufgabe hinzugefügt")
+
+
+def loeschen():
+    print("Was möchtest du löschen")
+    aufgabe = input()
+
+    if aufgabe in to_do_liste:
+        to_do_liste.remove(aufgabe)
+        print("Die aufgabe wurde gelöscht")
+
+    else:
+        print("Aufgabe nicht gefunden")
+
+
+#funktionen abrufen
+while True :
+    choice = to_do_list_menue()
+
+    if choice == "1":
+        anzeigen()
+
+    if choice == "2":
+        hinzufuegen()
+
+    if choice == "3":
+        loeschen()
+
+    if choice == "4":
+        print("Beenden")
+        break
+
+
+
 
